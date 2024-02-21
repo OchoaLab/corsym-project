c5a30b03f5183cdd00dd5a3bdde47379d9ad95ae
Alejandro Ochoa Garcia <alexviiiag@gmail.com>
2024-02-20 20:56:53 -0500

    code: added implementation and a small simulation that validates the function and demonstrate bias of Pearson when sample order is biased

---
+++ scripts/corsym.R
@@ -0,0 +1,45 @@
+# implementation of the method proposed in this paper
+
+corsym <- function( Q, q2 = NULL ) {
+    # validate inputs
+    if ( missing( Q ) )
+        stop( '`Q` is required!' )
+    if ( is.null( q2 ) ) {
+        # in this mode, Q must be a matrix with 2 columns
+        if ( !is.matrix( Q ) )
+            stop( 'When a single argument `Q` is provided, it must be a matrix!' )
+        if ( ncol( Q ) != 2 )
+            stop( '`Q` must have 2 columns!' )
+        # extract the two vectors of interest
+        q1 <- Q[ , 1 ]
+        q2 <- Q[ , 2 ]
+        n <- nrow( Q )
+    } else {
+        # in this case both need to be vectors of equal length
+        q1 <- Q # rename internally
+        # make sure length is equal
+        n <- length( q1 )
+        if( length( q2 ) != n )
+            stop( 'Both inputs must have the same length!' )
+    }
+    
+    # n only appears as 2*n...
+    nn <- 2 * n
+    
+    # estimate pooled mean
+    # this uses a bit less memory than concatenating q1 and q2 first
+    mu <- ( mean( q1 ) + mean( q2 ) ) / 2
+    # now estimate "base" pooled variance
+    sigma_sq <- ( sum( ( q1 - mu )^2 ) + sum( ( q2 - mu )^2 ) ) / ( nn - 1 )
+    # and "base" covariance estimate
+    covar <- sum( ( q1 - mu ) * ( q2 - mu ) ) * 2 / ( nn - 1 )
+
+    # now form small sample unbiased estimates
+    an <- ( nn - 1 ) ^2 / ( nn * ( nn - 2 ) )
+    bn <- an / ( nn - 1 )
+    sigma_sq_u <- an * sigma_sq + bn * covar
+    covar_u <- an * covar + bn * sigma_sq
+
+    # finally, estimate correlation and return!
+    return( covar_u / sigma_sq_u )
+}
