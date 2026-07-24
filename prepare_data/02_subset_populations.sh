0cb934fd0a61e1333df4dba3cf158cf22cc48aca
GKresearch <111245686+GKresearch@users.noreply.github.com>
2026-07-24 14:06:56 -0400

    Refactor file paths and enhance script description
    Updated file paths for keep files to use IN_DIR instead of WORK_DIR. Added additional context to the script description.

--- prepare_data/02_subset_populations.sh
+++ prepare_data/02_subset_populations.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-####Ran with Slurm using 50G memory and 1 CPU. This script takes both references and trio files and subsets each to only the populations of interest. The three references and the admixed population test cases
+####Ran with Slurm using 50G memory and 1 CPU. This script takes both references and trio files and subsets each to only the populations of interest. The three references and the admixed population test cases. Also, created id keep files to be used downstream in RFMix preparation.
 
 ###Load modules samtools, plink, and plink2
 export PATH="~/miniconda3/envs/samtools/bin:$PATH"
@@ -16,9 +16,9 @@ TEST_POPS='$1=="MXL" || $1=="PUR" || $1=="PEL" || $1=="CLM" || $1=="ASW" || $1==
 POP_NAME="HGDP_with_TGP_trios"
 
 ###Filter out non-reference populations from the reference plink file and convert to plink2, pgen format
-awk "$REF_POPS { print }" ${IN_FILE_REF}.psam > ${WORK_DIR}_ref_keep.ids
-plink2 --bfile ${IN_FILE_REF} --keep ${WORK_DIR}_ref_keep.ids --make-pgen --out ${IN_FILE_REF}_popsubset
+awk "$REF_POPS { print }" ${IN_FILE_REF}.psam > ${IN_DIR}/ref_keep.ids
+plink2 --bfile ${IN_FILE_REF} --keep ${IN_DIR}/ref_keep.ids --make-pgen --out ${IN_FILE_REF}_popsubset
 
 ###Filter out non-target populations from the trio plink2 file
-awk "$TEST_POPS { print }" ${IN_FILE_TRIO}.psam > ${WORK_DIR}_trio_keep.ids
-plink2 --pfile ${IN_FILE_TRIO} --keep ${WORK_DIR}_trio_keep.ids --make-pgen --out ${IN_FILE_TRIO}_popsubset
+awk "$TEST_POPS { print }" ${IN_FILE_TRIO}.psam > ${IN_DIR}/trio_keep.ids
+plink2 --pfile ${IN_FILE_TRIO} --keep ${IN_DIR}/trio_keep.ids --make-pgen --out ${IN_FILE_TRIO}_popsubset
