1833c898b7c4df4cdb972ec5a350106d2fd4f01c
GKresearch <111245686+GKresearch@users.noreply.github.com>
2026-07-29 14:19:34 -0400

    Implement reorder_proportion_p function
    Add a function to reorder data based on proportions.

--- analyses/simulation_analysis_functions.R
+++ analyses/simulation_analysis_functions.R
@@ -1,1 +1,30 @@
+#
+reorder_proportion_p <- function(df, v1, v2, prop = .5) {
 
+  idx <- sample(nrow(df), size = floor(prop * nrow(df)))
+  reordered <- slice(df, idx)
+  unordered  <- slice(df, -idx)
+
+  reordered<-reordered%>%
+    mutate(
+      !!paste0(v1,".",as.character(prop)) := case_when(
+        .data[[v1]] > .data[[v2]] ~ .data[[v1]],
+        .data[[v2]] > .data[[v1]] ~ .data[[v2]],
+        .data[[v2]] == .data[[v1]] ~ .data[[v1]]
+      ),
+      !!paste0(v2,".",as.character(prop)) := case_when(
+        .data[[v1]] > .data[[v2]] ~ .data[[v2]],
+        .data[[v2]] > .data[[v1]] ~ .data[[v1]],
+        .data[[v2]] == .data[[v1]] ~ .data[[v2]]
+      )
+    )
+  unordered<-unordered%>%
+    mutate(
+      !!paste0(v1,".",as.character(prop)) := .data[[v1]],
+      !!paste0(v2,".",as.character(prop)) := .data[[v2]],
+      )
+  
+  df <- bind_rows(reordered, unordered) 
+  
+  return(df)
+}
