00d7883dbec99938fd275aa4bb8b90ed6f55222e
GKresearch <111245686+GKresearch@users.noreply.github.com>
2026-08-10 11:23:26 -0400

    Fix column name assignment in randomize_order_filter

--- analyses/functions/empirical_analysis_functions.R
+++ analyses/functions/empirical_analysis_functions.R
@@ -22,7 +22,7 @@ run_pair <- function(dat, col1, col2, fun) {
 #Small Function to ensure randomize_order works correctly
 randomize_order_filter <- function(df, col1, col2){
   df_randomize <- as.data.frame(randomize_order(as.matrix(df[c(col1,col2)])))
-  colnames(df_randomize)<-c(paste0(col1,".rand"),paste0(col2.rand))
+  colnames(df_randomize)<-c(paste0(col1,".rand"),paste0(col2,".rand"))
   df<-cbind(df,df_randomize)
   return(df)
 }
