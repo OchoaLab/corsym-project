1a3e07d95ca667648b8168c6e8801943c024d2e5
GKresearch <111245686+GKresearch@users.noreply.github.com>
2026-08-11 14:07:46 -0400

    Implement KS tests and visualizations for parent comparison
    Added statistical tests and visualizations for true parent comparisons across different populations.

--- analyses/empirical/03_true_parent_comparison_plot.R
+++ analyses/empirical/03_true_parent_comparison_plot.R
@@ -97,3 +97,88 @@ p_fig3<-(p_fig3a|p_fig3b|p_fig3c)+
 ggsave("Figure3.pdf",p_fig3,
        dpi=600,width=29.5,height=20,units="in")
 
+
+
+######Old Tests
+ks_test_set<-df2%>%
+  select(c(a3.p1, a3.p2,coh))%>%
+  pivot_longer(
+    cols = c(a3.p1, a3.p2),
+    names_to = "Parent",
+    values_to = "value"
+  )%>%
+  mutate(Parent=case_when(
+    Parent=="a3.p1"~"Father",
+    Parent=="a3.p2"~"Mother",
+    TRUE ~ "NA")
+  )%>%
+  filter(!is.na(value))
+for(pop in unique(ks_test_set$coh)){
+  ks_test_output<-ks.test(subset(ks_test_set,(coh==pop&Parent=="Father"))$value,
+          subset(ks_test_set,(coh==pop&Parent=="Mother"))$value,alternative = "two.sided")
+  print(paste0(pop," for YRI had a p-value of: ",ks_test_output$p.value))
+}
+
+ks_test_set<-df2%>%
+  select(c(a2.p1, a2.p2,coh))%>%
+  pivot_longer(
+    cols = c(a2.p1, a2.p2),
+    names_to = "Parent",
+    values_to = "value"
+  )%>%
+  mutate(Parent=case_when(
+    Parent=="a2.p1"~"Father",
+    Parent=="a2.p2"~"Mother",
+    TRUE ~ "NA")
+  )%>%
+  filter(!is.na(value))
+for(pop in unique(ks_test_set$coh)){
+  ks_test_output<-ks.test(subset(ks_test_set,(coh==pop&Parent=="Father"))$value,
+          subset(ks_test_set,(coh==pop&Parent=="Mother"))$value,alternative = "two.sided")
+  print(paste0(pop," for IBS had a p-value of: ",ks_test_output$p.value))
+}
+
+ks_test_set<-df2%>%
+  select(c(a1.p1, a1.p2,coh))%>%
+  pivot_longer(
+    cols = c(a1.p1, a1.p2),
+    names_to = "Parent",
+    values_to = "value"
+  )%>%
+  mutate(Parent=case_when(
+    Parent=="a1.p1"~"Father",
+    Parent=="a1.p2"~"Mother",
+    TRUE ~ "NA")
+  )%>%
+  filter(!is.na(value))
+for(pop in unique(ks_test_set$coh)){
+  ks_test_output<-ks.test(subset(ks_test_set,(coh==pop&Parent=="Father"))$value,
+          subset(ks_test_set,(coh==pop&Parent=="Mother"))$value,alternative = "two.sided")
+  print(paste0(pop," for NAM had a p-value of: ",ks_test_output$p.value))
+}
+0.05/18
+
+p1<-df2%>%
+  select(c(a3.p1, a3.p2,coh))%>%
+  pivot_longer(
+    cols = c(a3.p1, a3.p2),
+    names_to = "Parent",
+    values_to = "value"
+  )%>%
+  mutate(Parent=case_when(
+    Parent=="a3.p1"~"Father",
+    Parent=="a3.p2"~"Mother",
+    TRUE ~ "NA")
+  )%>%
+  filter(!is.na(value))%>%
+  ggplot(.)+
+  geom_boxplot(aes(y=`value`,fill=Parent,x=coh,col=after_scale(fill)),size=2,alpha=.5)+
+  labs(y="True Ancestry\n(YRI-Like)",x="Population")+
+  scale_fill_manual(name="Parent", values=c("#00FFCC","#FF9933"))+
+  theme_professional(base_size = 30)+
+  theme(legend.position = "bottom",panel.border=element_rect(linewidth = 3))
+
+ggplot(df2)+
+  geom_histogram(aes(a3.p1),fill="#00FFCC")+
+    geom_histogram(aes(a3.p2),fill="#FF9933")+
+  facet_wrap(.~coh)
