6a3df53f0b9839c7d59922b7a76651dcb0f62385
GKresearch <111245686+GKresearch@users.noreply.github.com>
2026-08-16 16:01:11 -0400

    Add script for preparing empirical dataset for plotting
    This script prepares data for plotting figures on the empirical dataset and outputs analysis datasets.

---
+++ analyses/empirical/06_stats.R
@@ -0,0 +1,17 @@
+##This script prepares data for plotting various figures on the empirical dataset.
+#This script will output a "Two-way" and "Three-way" analysis dataset with all important information held within
+
+source( "empirical_analysis_functions.R" )
+source( 'plotting_functions.R' )
+
+# load libraries
+library(tidyr)
+library(dplyr)
+library(corsym)
+library(ggplot2)
+library(RColorBrewer)
+library(ggh4x)
+
+############################
+#########Plot Figure 4
+############################
